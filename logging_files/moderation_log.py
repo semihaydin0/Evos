198ec014277a2a2a751decfb12c2e769f8cf507e
Semih Aydın <68512128+semihaydin0@users.noreply.github.com>
2020-11-10 11:20:14 +0300

    Add files via upload

--- logging_files/moderation_log.py
+++ logging_files/moderation_log.py
@@ -6,7 +6,7 @@ logger.setLevel(logging.INFO)
 
 formatter = logging.Formatter("%(asctime)s:%(levelname)s:%(message)s")
 
-file_handler = logging.FileHandler("./logs/admin.log")
+file_handler = logging.FileHandler("./logs/moderation.log")
 file_handler.setFormatter(formatter)
 
 logger.addHandler(file_handler)
