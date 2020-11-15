d6aa3b5024295fb4074917dc469e114b4a646b46
Semih Aydın <68512128+semihaydin0@users.noreply.github.com>
2020-11-16 00:31:14 +0300

    Informative comment lines added

--- cogs/Error.py
+++ cogs/Error.py
@@ -1,3 +1,7 @@
+#MIT License
+#Copyright (c) 2020 Semih Aydın
+#UTF-8
+
 import discord
 from discord.ext import commands
 
@@ -9,17 +13,28 @@ class Error(commands.Cog):
     
     @commands.Cog.listener()
     async def on_command_error(self,ctx,error):
+        """CommandNotFound"""
         if isinstance(error , commands.CommandNotFound):
             pass
+
             logger.info(f"Error | Komut Bulunamadı : {ctx.message.content} | Tarafından : {ctx.author}")
+
+        """MissingRequiredArgument"""
         if isinstance(error , commands.MissingRequiredArgument):
             await ctx.send(f"{ctx.author.mention} Eksik argüman girişi. **.help <komut>** yazıp komut için gerekli olan argümana ulaşabilirsin.")
+
             logger.info(f"Error | Komut : {ctx.message.content} | Eksik Argüman Tespiti : {ctx.author}")
+
+        """MissingPermissions"""
         if isinstance(error , commands.MissingPermissions):
             await ctx.send(f"{ctx.author.mention} Bu komut için yeterli yetkiye sahip değilsin. **.yardım** yazarak diğer komutlara göz atabilirsin.")
+
             logger.info(f"Error | Komut : {ctx.message.content} | Eksik Yetki : {ctx.author}")
+
+        """BotMissingPermissions"""
         if isinstance(error, commands.BotMissingPermissions):
             await ctx.send(f"{ctx.author.mention} Bu komut için Evos'un **yönetici** rolüne ihtiyacı var. Roller kısmından Evos rolüne yönetici izni vermeni öneriyoruz.")
+
             logger.info(f"Error | Komut : {ctx.message.content} | Evos yeterli yetkiye sahip değil : {ctx.guild.name}")
 
 def setup(client):
