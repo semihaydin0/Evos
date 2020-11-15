50db853996a4cd8197ba4f1377245a2117df8f98
Semih Aydın <68512128+semihaydin0@users.noreply.github.com>
2020-11-16 00:24:08 +0300

    Informative comment lines added

--- cogs/Admin.py
+++ cogs/Admin.py
@@ -1,3 +1,7 @@
+#MIT License
+#Copyright (c) 2020 Semih Aydın
+#UTF-8
+
 import os
 import discord
 from discord.ext import commands
@@ -11,7 +15,11 @@ class Admin(commands.Cog):
     @commands.is_owner()
     @commands.command(name = "load",aliases=['yükle'],hidden=True)
     async def load_command(self,ctx,module :str):
+        """Loads Module
+        Use of : load {module_name}
+        """
         status = False
+
         for filename in os.listdir('./cogs'):
             if filename.endswith('.py'):
                 if module == filename[:-3] :
@@ -19,20 +27,26 @@ class Admin(commands.Cog):
                         self.client.load_extension(f'cogs.{filename[:-3]}')
                         load_embed=discord.Embed(title=f"{filename[:-3]} adlı modül yüklendi.",colour=0xffd500)
                         await ctx.send(embed=load_embed)
-                        logger.info(f"Admin | Modül Yükleme | Tarafından : {ctx.author}")
                         status=True
+
+                        logger.info(f"Admin | Modül Yükleme | Tarafından : {ctx.author}")
                     except :
                         loader_embed=discord.Embed(title=f"{filename[:-3]} adlı modül yüklenemedi.",description ="Modül halihazırda yüklenmiş olabilir.",colour=0xffd500)
                         await ctx.send(embed=loader_embed)
                         status=True
+
         if status==False :
             loader2_embed=discord.Embed(title=f"{module} adlı modül bulunamadı.",colour=0xffd500)
-            await ctx.send(embed=loader2_embed)         
+            await ctx.send(embed=loader2_embed)
 
     @commands.is_owner()
     @commands.command(name="unload",aliases=['kaldır'],hidden=True)
     async def unload_command(self,ctx,module :str):
+        """Unloads Module
+        Use of : unload {module_name}
+        """
         status = False
+
         for filename in os.listdir('./cogs'):
             if filename.endswith('.py'):
                 if module == filename[:-3] :
@@ -40,12 +54,14 @@ class Admin(commands.Cog):
                         self.client.unload_extension(f'cogs.{filename[:-3]}')
                         unload_embed=discord.Embed(title=f"{filename[:-3]} adlı modül kaldırıldı.",colour=0xffd500)
                         await ctx.send(embed=unload_embed)
-                        logger.info(f"Admin | Modül Kaldırma | Tarafından : {ctx.author}")
                         status=True
+
+                        logger.info(f"Admin | Modül Kaldırma | Tarafından : {ctx.author}")
                     except :
                         unloader_embed=discord.Embed(title=f"{filename[:-3]} adlı modül kaldırılamadı.",description ="Modül halihazırda kaldırılmış olabilir.",colour=0xffd500)
                         await ctx.send(embed=unloader_embed)
                         status=True
+
         if status==False :
             loader2_embed=discord.Embed(title=f"{module} adlı modül bulunamadı.",colour=0xffd500)
             await ctx.send(embed=loader2_embed)
@@ -53,7 +69,11 @@ class Admin(commands.Cog):
     @commands.is_owner()
     @commands.command(name="reload",aliases=['tekraryükle'],hidden=True)
     async def reload_command(self,ctx,module :str):
+        """Reloads Module
+        Use of : reload {module_name}
+        """
         status = False
+
         for filename in os.listdir('./cogs'):
             if filename.endswith('.py'):
                 if module == filename[:-3] :
@@ -62,12 +82,14 @@ class Admin(commands.Cog):
                         self.client.load_extension(f'cogs.{filename[:-3]}')
                         reload_embed=discord.Embed(title=f"{filename[:-3]} adlı modül yeniden yüklendi.",colour=0xffd500)
                         await ctx.send(embed=reload_embed)
-                        logger.info(f"Admin | Yeniden Modül Yükleme | Tarafından : {ctx.author}")
                         status=True
+
+                        logger.info(f"Admin | Yeniden Modül Yükleme | Tarafından : {ctx.author}")
                     except :
                         reloader_embed=discord.Embed(title=f"{filename[:-3]} adlı modül yeniden yüklenemedi.",colour=0xffd500)
                         await ctx.send(embed=reloader_embed)
                         status=True
+
         if status==False :
             loader2_embed=discord.Embed(title=f"{module} adlı modül bulunamadı.",colour=0xffd500)
             await ctx.send(embed=loader2_embed)                
@@ -75,7 +97,11 @@ class Admin(commands.Cog):
     @commands.is_owner()
     @commands.command(name="log",aliases=['günlük'],hidden=True)
     async def log_command(self, ctx,name):
+        """Send The Logs
+        Use of : log {log_name}
+        """
         status = False
+
         for filename in os.listdir('./logs'):
             if filename.endswith('.log'):
                 if name+".log" == filename:
@@ -84,6 +110,9 @@ class Admin(commands.Cog):
                     log_embed=discord.Embed(title=f"{name} adlı günlük kaydı DM üzerinden gönderildi.",colour=0xffd500)
                     await ctx.send(embed=log_embed)
                     status=True
+
+                    logger.info(f"Admin | Günlük Kaydı : {name} | Tarafından : {ctx.author}")
+
         if status==False :
             loger_embed=discord.Embed(title=f"{name} adlı günlük kaydı bulunamadı.",colour=0xffd500)
             await ctx.send(embed=loger_embed)
@@ -91,23 +120,34 @@ class Admin(commands.Cog):
     @commands.is_owner()
     @commands.command(name="activity",aliases=['aktivite'],hidden=True)
     async def activity_command(self, ctx,name):
+        """Changing Activity
+        Use of : activity {name}
+        """
         activity = ".yardım | "
+
         if name == "default" :
             activity+="🎵 NEW HIGH QUALITY MUSIC"
         else :
             name = name.replace("."," ")
             activity += name
+
         await self.client.change_presence(status=discord.Status.online , activity=discord.Game(f"{activity}"))           
         activity_embed=discord.Embed(title="Aktivite değişikliği başarılı.",colour=0xffd500)
         await ctx.send(embed=activity_embed)
+
         logger.info(f"Admin | Aktivite Değişikliği : {name} | Tarafından : {ctx.author}")
 
     @commands.is_owner()
     @commands.command(name="off",aliases=['kapat'],hidden=True)
     async def off_command(self, ctx):
+        """End The Bot Session
+        Use of : off
+        """
         off_embed=discord.Embed(title=f"{self.client.user.name} kapatılıyor.",colour=0xffd500)
         await ctx.send(embed=off_embed)
+
         logger.info(f"Admin | Kapatma Talebi | Tarafından : {ctx.author}")
+
         await self.client.logout()
 
 def setup(client):
